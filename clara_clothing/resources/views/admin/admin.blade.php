139c3f41518a17937a70f7b549f0ad636a108354
WarunaDisanayaka <warunahello@gmail.com>
2022-10-30 20:35:06 +0530

    Admin dashboard cards

--- clara_clothing/resources/views/admin/admin.blade.php
+++ clara_clothing/resources/views/admin/admin.blade.php
@@ -2,4 +2,16 @@
 
 @section('content')
 
+<section class="admin-cards">
+    <div class="values">
+        <div class="val-box">
+            <i class="fas fa-users"></i>
+            <div>
+                <h3>8273</h3>
+                <span>New Users</span>
+            </div>
+        </div>
+    </div>
+</section>
+
 @endsection
--- clara_clothing/resources/views/layouts/main.blade.php
+++ clara_clothing/resources/views/layouts/main.blade.php
@@ -19,6 +19,7 @@
   <meta content="" name="keywords">
 
 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
