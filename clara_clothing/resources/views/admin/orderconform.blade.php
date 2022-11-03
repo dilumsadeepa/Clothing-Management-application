1dc035f4c0c8e0d5dc3c4607daa9a9535ca1a15f
Firstname Lastname <asupunjayasooriya@gmail.com>
2022-11-03 16:31:03 +0530

    Update orderconform.blade.php

--- clara_clothing/resources/views/admin/orderconform.blade.php
+++ clara_clothing/resources/views/admin/orderconform.blade.php
@@ -24,10 +24,17 @@
                 <td>{{$o->products}}</td>
                 <td>{{$o->total}}</td>
                 <td>{{$o->confirm}}</td>
-                <td>
-                </td>
+                
                 <td>
                     <form action="{{ route('ordercon.destroy',$o->id) }}" method="POST">
+
+                        <label for="color">Satatus</label>
+                    <select name="color" id="color">
+                        <option value="">Select</option>
+                        <option value="red">Red</option>
+                        <option value="green">Green</option>
+                        <option value="blue">Blue</option>
+                    </select>
 
                         <a class="btn btn-primary" href="{{ route('ordercon.edit',$o->id) }}">Edit</a>
 
